6e3eeb13bf33ba3c06713188b8d4d51b744b08f4
bashytech007 <bashiraremu474@gmail.com>
2023-09-16 19:00:21 +0100

    sixth-commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Routes, Route} from "react-router-dom";
 import Home from './pages/Home';
 import Search from './pages/Search';
 import MovieDetails from './pages/MovieDetails';
-import SideBar from './components/Sidebar';
+
 
 
 function App() {
